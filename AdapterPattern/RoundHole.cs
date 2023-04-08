c88399967f0ca448ff833a8739d2b8b5d3ab0128
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-08 15:45:44 +0300

    Created classes "RoundHole" and "RoundPeg"

---
+++ AdapterPattern/RoundHole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AdapterPattern;
+
+public class RoundHole
+{
+    public uint radius { get; private set; }
+    public RoundHole(uint radius) => this.radius = radius;
+    public bool Fits(RoundPeg peg)
+    {
+        return this.radius >= peg.radius;
+    }
+}
---
+++ AdapterPattern/RoundPeg.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class RoundPeg
+{
+    public uint radius { get; private set; }
+    public RoundPeg(uint radius) => this.radius = radius;
+}
