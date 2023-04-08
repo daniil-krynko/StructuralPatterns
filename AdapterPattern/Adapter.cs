bddbd32edc38572943155d9976bc888dbf98e3aa
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-08 15:47:07 +0300

    Created class "Adapter"

---
+++ AdapterPattern/Adapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using AdapterPattern;
+public class Adapter : RoundPeg
+{
+    private SquarePeg peg;
+    public Adapter(SquarePeg peg) : base(Convert.ToUInt32(peg.width * Math.Sqrt(2) / 2))
+    {
+        this.peg = peg;
+    }
+}
