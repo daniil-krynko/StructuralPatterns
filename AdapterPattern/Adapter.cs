022315b230532d74d68cb948107467bf2987f428
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-08 16:04:21 +0300

    Written demonstration in main

--- AdapterPattern/Adapter.cs
+++ AdapterPattern/Adapter.cs
@@ -3,7 +3,7 @@ using AdapterPattern;
 public class Adapter : RoundPeg
 {
     private SquarePeg peg;
-    public Adapter(SquarePeg peg) : base(Convert.ToUInt32(peg.width * Math.Sqrt(2) / 2))
+    public Adapter(SquarePeg peg) : base(Convert.ToUInt32(peg.width * Math.Sqrt(2) / 2)) // Call of parent's constructor
     {
         this.peg = peg;
     }
--- AdapterPattern/Program.cs
+++ AdapterPattern/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Example of Adapter pattern\n");
+
+            RoundHole hole = new RoundHole(5);
+            RoundPeg rpeg = new RoundPeg(5);
+            Console.WriteLine($"Does round peg fits to hole? - {hole.Fits(rpeg)}");
+
+            SquarePeg smallSqpeg = new SquarePeg(5);
+            SquarePeg largeSqpeg = new SquarePeg(10);
+            // hole.Fits(smallSqpeg); // It won't compile (incompatible types)
+
+            Adapter smallSqpegAdapter = new Adapter(smallSqpeg);
+            Adapter largeSqpegAdapter = new Adapter(largeSqpeg);
+            Console.WriteLine($"Does small square peg fits to hole? - {hole.Fits(smallSqpegAdapter)} ({smallSqpegAdapter.radius})"); // true
+            Console.WriteLine($"Does large square peg fits to hole? - {hole.Fits(largeSqpegAdapter)} ({largeSqpegAdapter.radius})"); // false
         }
     }
 }
