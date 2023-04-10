9f36a2bf667a5432cec96f0256df0809024110b0
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-10 11:39:06 +0300

    Added some comments

--- AdapterPattern/Adapter.cs
+++ AdapterPattern/Adapter.cs
@@ -3,6 +3,7 @@ using AdapterPattern;
 public class Adapter : RoundPeg
 {
     private SquarePeg peg;
+    // peg.width * Math.Sqrt(2) / 2) - формула описанной окружности квадрата
     public Adapter(SquarePeg peg) : base(Convert.ToUInt32(peg.width * Math.Sqrt(2) / 2)) // Call of parent's constructor
     {
         this.peg = peg;
